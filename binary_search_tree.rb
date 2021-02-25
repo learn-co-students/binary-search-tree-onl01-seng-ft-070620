a9c1edf372b1fe33f3322792d2ac787c571bb4db
bbbtttiii <ben.iburg@gmail.com>
2021-02-25 10:27:25 -0600

    Done.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class BST 
   attr_accessor :data, :left, :right
 
@@ -6,19 +8,19 @@ class BST
     @left = nil
     @right = nil
   end
-
-  def each(&block)
-    self.left.each(&block) if self.left
-    yield self.data
-    self.right.each(&block) if self.right
-  end
-
+  
   def insert(data)
     if data <= self.data
       self.left ? self.left.insert(data) : self.left = BST.new(data)
     else
       self.right ? self.right.insert(data) : self.right = BST.new(data)
     end
+  end
+  
+  def each(&block)
+    self.left.each(&block) if self.left
+    yield self.data
+    self.right.each(&block) if self.right
   end
 
 end
